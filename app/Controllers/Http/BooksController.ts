0550e0e04de720e09d187497d3157213fee947fd
agung <agung@inkorgroup.com>
2022-09-27 11:12:21 +0700

    update to raw query

--- app/Controllers/Http/BooksController.ts
+++ app/Controllers/Http/BooksController.ts
@@ -1,6 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Book from 'App/Models/Book'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class BooksController {
   public async create(ctx: HttpContextContract) {
@@ -21,15 +22,26 @@ export default class BooksController {
   }
 
   public async getAll(ctx: HttpContextContract) {
-    const authors = await Book.all()
-    return ctx.response.status(200).json({ code: 200, status: 'success', data: authors })
+    const query = `select b.*, a.name as author_name , p.name as publisher_name
+    from books as b
+      inner join authors as a on b.author_id = a.id
+      inner join publishers as p on b.publisher_id = p.id`
+    const authors = await Database.rawQuery(query)
+    // const authors = await Book.all()
+    return ctx.response.status(200).json({ code: 200, status: 'success', data: authors[0] })
   }
 
   public async getById(ctx: HttpContextContract) {
     const id = ctx.params.id
+    const query = `select b.*, a.name as author_name , p.name as publisher_name
+    from books as b
+      inner join authors as a on b.author_id = a.id
+      inner join publishers as p on b.publisher_id = p.id
+    where b.id = ?`
     try {
-      const author = await Book.findBy('id', id)
-      return ctx.response.status(200).json({ code: 200, status: 'success', data: author })
+      // const author = await Book.findBy('id', id)
+      const author = await Database.rawQuery(query, [id])
+      return ctx.response.status(200).json({ code: 200, status: 'success', data: author[0] })
     } catch (err) {
       return ctx.response.status(500).json({ code: 500, status: 'error', message: err.message })
     }
